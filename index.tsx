abb22c84a886119db0f773e2eb7e6ad25793e69a
AcousticCats <eduardoredeelite@gmail.com>
2025-11-24 23:24:15 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,28 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// Registro do Service Worker para PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then((registration) => {
-        console.log('ServiceWorker registrado com sucesso: ', registration.scope);
-      })
-      .catch((err) => {
-        console.log('Falha ao registrar ServiceWorker: ', err);
-      });
-  });
-}
