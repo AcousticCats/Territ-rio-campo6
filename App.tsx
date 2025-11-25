24b61260dd9810535601726c6c333daacbe1cfed
AcousticCats <eduardoredeelite@gmail.com>
2025-11-24 23:24:56 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,35 +1,11 @@
-import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AppProvider } from './context/AppContext';
-import { LoginScreen } from './screens/LoginScreen';
-import { RegisterScreen } from './screens/RegisterScreen';
-import { TerritoryListScreen } from './screens/TerritoryListScreen';
-import { MapScreen } from './screens/MapScreen';
-import { NotesScreen } from './screens/NotesScreen';
-import { ReportScreen } from './screens/ReportScreen';
-import { SettingsScreen } from './screens/SettingsScreen';
+// App.tsx
+import React from "react";
+import TerritoryListScreen from "./screens/TerritoryListScreen";
 
-const App = () => {
+export default function App() {
   return (
-    <AppProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<LoginScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/home" element={<TerritoryListScreen />} />
-          <Route path="/map/:id" element={<MapScreen />} />
-          <Route path="/notes/:id" element={<NotesScreen />} />
-          <Route path="/reports" element={<ReportScreen />} />
-          <Route path="/settings" element={<SettingsScreen />} />
-          
-          {/* Fallback routes */}
-          <Route path="/global-map" element={<Navigate to="/home" replace />} />
-          <Route path="/profile" element={<Navigate to="/settings" replace />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </HashRouter>
-    </AppProvider>
+    <div style={{ width: "100%", height: "100%" }}>
+      <TerritoryListScreen />
+    </div>
   );
-};
-
-export default App;
+}
